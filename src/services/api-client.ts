a07ab2666a9d15b95d63ae2b1e80d88861c1d3dd
aleksander <aleksbro@stud.ntnu.no>
2023-09-24 14:29:43 +0200

    Fetch game details

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -14,15 +14,21 @@ const axiosInstance = axios.create({
 });
 
 class APIClient<T> {
-  enpoint: string;
+  endpoint: string;
 
-  constructor(endpint: string) {
-    this.enpoint = endpint;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
   }
 
   getAll = (config: AxiosRequestConfig) => {
     return axiosInstance
-      .get<FetchResponse<T>>(this.enpoint, config)
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
       .then((res) => res.data);
   };
 }
